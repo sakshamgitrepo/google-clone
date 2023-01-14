937f7220bb7f81b3900ee44c0bf6bc93bb1275a4
Saksham <sakshamraj61@gmail.com>
2023-01-15 04:10:24 +0530

    homepage is complete

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Home from "./components/Home"
+import SearchResult from "./components/SearchResult"
+
+function App() {
+
+  return (
+<BrowserRouter>
+<Routes>
+<Route path="/"  element={<Home/>}/>
+<Route path="/search"  element={<SearchResult/>}/>
+</Routes>
+</BrowserRouter>
+  )
+}
+
+export default App
---
+++ src/utils/Constants.jsx
@@ -0,0 +1,7 @@
+export const quickLinks = [
+    "About",
+    "Advertising",
+    "Business",
+    "How Search works",
+];
+export const settingMenu = ["Privacy", "Terms", "Settings"];
